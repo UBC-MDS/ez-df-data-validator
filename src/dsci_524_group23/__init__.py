872e3a5bb1d653a8defe394c3e1bd8c57d05b2d6
johnsonsyleung <johnsonsyleung@gmail.com>
2026-01-23 23:48:48 -0800

    Update __init__.py

--- src/dsci_524_group23/__init__.py
+++ src/dsci_524_group23/__init__.py
@@ -31,6 +31,6 @@ utilities for machine learning workflows.
 from .schema_standardizer import standardize_schema as standardize_schema
 from .find_duplicates import find_duplicates as find_duplicates
 from .handle_missing import handle_missing as handle_missing
-from.missing_summary import missing_summary as missing_summary
+from .missing_summary import missing_summary as missing_summary
 
 __version__ = "0.1.0"
